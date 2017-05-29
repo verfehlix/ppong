2f82c80db8280728484c980e7e32b3eed39cceed
Felix Magnus <felix.p.magnus@gmail.com>
2017-05-29 17:29:38 +1000

    goal text and layering

--- ppong.js
+++ ppong.js
@@ -4,6 +4,10 @@ let game = new Phaser.Game(800,600,Phaser.AUTO,'',{
     update: update
 }, false, false)
 
+let layerBackground
+let layerGameObjects
+let layerText
+
 let paddle1
 let paddle2
 let ball
@@ -24,8 +28,11 @@ let player2WinEmitter
 let scorePlayer1
 let scorePlayer2
 
-let bmpTextScorePlayer1
-let bmpTextScorePlayer2
+let scoreTextPlayer1
+let scoreTextPlayer2
+
+let goalTextPlayer1
+let goalTextPlayer2
 
 function preload() {
     // background & decorations
@@ -62,15 +69,22 @@ function preload() {
 }
 
 function create() {
+    // create groups for layering sprites
+    layerBackground = game.add.group();
+    layerGameObjects = game.add.group();
+    layerText = game.add.group();
+    
     // create background stuff
     let bg = game.add.tileSprite(-100, -100, 1000, 800, "bg")
     bg.alpha = 0.4
+    layerBackground.add(bg)
 
     let midLine1 = game.add.tileSprite(game.world.width / 2, game.world.height / 4, 16, 268, 'line_green')
     midLine1.anchor.setTo(0.5,0.5)
     midLine1.alpha = 1
     midLine1.animations.add('lineMove')
     midLine1.animations.play('lineMove', 100, true)
+    layerBackground.add(midLine1)
 
     let midLine2 = game.add.tileSprite(game.world.width / 2, game.world.height * 3/4, 16, 268, 'line_green')
     midLine2.anchor.setTo(0.5,0.5)
@@ -78,15 +92,18 @@ function create() {
     midLine2.angle = 180
     midLine2.animations.add('lineMove')
     midLine2.animations.play('lineMove', 100, true)
+    layerBackground.add(midLine2)
 
     let midPoint = game.add.sprite(game.world.width / 2, game.world.height / 2,"mid_point")
     midPoint.anchor.setTo(0.5,0.5)
+    layerBackground.add(midPoint)
 
     let leftLine = game.add.tileSprite(16, game.world.height / 2, 16, 568, 'line_blue')
     leftLine.anchor.setTo(0.5,0.5)
     leftLine.alpha = 1
     leftLine.animations.add('lineMove')
     leftLine.animations.play('lineMove', 100, true)
+    layerBackground.add(leftLine)
 
     let rightLine = game.add.tileSprite(784, game.world.height / 2, 16, 568, 'line_pink')
     rightLine.anchor.setTo(0.5,0.5)
@@ -94,6 +111,7 @@ function create() {
     rightLine.angle = 180
     rightLine.animations.add('lineMove')
     rightLine.animations.play('lineMove', 100, true)
+    layerBackground.add(rightLine)
 
     let topLine = game.add.tileSprite(game.world.width / 2, 16, 16, 768, 'line_green')
     topLine.anchor.setTo(0.5,0.5)
@@ -101,6 +119,7 @@ function create() {
     topLine.angle = 90
     topLine.animations.add('lineMove')
     topLine.animations.play('lineMove', 100, true)
+    layerBackground.add(topLine)
 
     let bottomLine = game.add.tileSprite(game.world.width / 2, 584, 16, 768, 'line_green')
     bottomLine.anchor.setTo(0.5,0.5)
@@ -108,38 +127,48 @@ function create() {
     bottomLine.angle = 270
     bottomLine.animations.add('lineMove')
     bottomLine.animations.play('lineMove', 100, true)
+    layerBackground.add(bottomLine)
 
     let squareTopLeft = game.add.sprite(16,16,"square")
     squareTopLeft.anchor.setTo(0.5,0.5)
+    layerBackground.add(squareTopLeft)
 
     let squareTopRight = game.add.sprite(784,16,"square")
     squareTopRight.anchor.setTo(0.5,0.5)
+    layerBackground.add(squareTopRight)
 
     let squareBottomRight = game.add.sprite(784,584,"square")
     squareBottomRight.anchor.setTo(0.5,0.5)
+    layerBackground.add(squareBottomRight)
 
     let squareBottomLeft = game.add.sprite(16,584,"square")
     squareBottomLeft.anchor.setTo(0.5,0.5)
+    layerBackground.add(squareBottomLeft)
 
     let squareMidTop = game.add.sprite(game.world.width / 2,16,"square")
     squareMidTop.anchor.setTo(0.5,0.5)
+    layerBackground.add(squareMidTop)
 
     let squareMideBottom = game.add.sprite(game.world.width / 2,584,"square")
     squareMideBottom.anchor.setTo(0.5,0.5)
+    layerBackground.add(squareMideBottom)
 
     let squareKickoff = game.add.sprite(game.world.width / 2,game.world.height / 2,"square")
     squareKickoff.anchor.setTo(0.5,0.5)
+    layerBackground.add(squareKickoff)
 
     // reset scores
     scorePlayer1 = 0
     scorePlayer2 = 0
 
     // display scores
-    bmpTextScorePlayer1 = game.add.bitmapText(100, 125, 'mecha_blue', '' + scorePlayer1, 500)
-    bmpTextScorePlayer1.alpha = 0.1
+    scoreTextPlayer1 = game.add.bitmapText(100, 125, 'mecha_blue', '' + scorePlayer1, 500)
+    scoreTextPlayer1.alpha = 0.1
+    layerBackground.add(scoreTextPlayer1)
 
-    bmpTextScorePlayer2 = game.add.bitmapText(475, 125, 'mecha_pink', '' + scorePlayer2, 500)
-    bmpTextScorePlayer2.alpha = 0.15
+    scoreTextPlayer2 = game.add.bitmapText(475, 125, 'mecha_pink', '' + scorePlayer2, 500)
+    scoreTextPlayer2.alpha = 0.15
+    layerBackground.add(scoreTextPlayer2)
 
     // create particle emitters for winning explosion
     player1WinEmitter = game.add.emitter(game.world.centerX, game.world.centerY, 500)
@@ -148,6 +177,8 @@ function create() {
     player1WinEmitter.setAlpha(1, 0, 2000)
     player1WinEmitter.setXSpeed(-1000,1000)
     player1WinEmitter.setYSpeed(-1000,1000)
+    layerGameObjects.add(player1WinEmitter)
+
 
     player2WinEmitter = game.add.emitter(game.world.centerX, game.world.centerY, 500)
     player2WinEmitter.makeParticles(['particle1_pink','particle2_pink','particle3_pink'])
@@ -155,10 +186,13 @@ function create() {
     player2WinEmitter.setAlpha(1, 0, 2000)
     player2WinEmitter.setXSpeed(-1000,1000)
     player2WinEmitter.setYSpeed(-1000,1000)
+    layerGameObjects.add(player2WinEmitter)
 
     // create walls
     topBottomWalls = createCollisionGroup()
     sideWalls = createCollisionGroup()
+    layerGameObjects.add(topBottomWalls)
+    layerGameObjects.add(sideWalls)
 
     createWall(0,-25,topBottomWalls,800,25,"top")
     createWall(0,600,topBottomWalls,800,25,"bottom")
@@ -168,18 +202,21 @@ function create() {
 
     // create paddles / players
     paddles = createCollisionGroup()
+    layerGameObjects.add(paddles)
 
     paddle1 = createPaddle(55,game.world.centerY,paddles,"player1")
     paddle2 = createPaddle(game.world.width - 55,game.world.centerY,paddles,"player2")
 
     // create ball
     ball = createBall(game.world.centerX, game.world.centerY)
-    
+    layerGameObjects.add(ball)
+
     // create emitters for the ball
     wallBounceEmitter = game.add.emitter(-1000, -1000, 500)
     wallBounceEmitter.makeParticles(['particle1_green','particle2_green','particle3_green'])
     wallBounceEmitter.setScale(2, 0, 2, 0, 1000)
     wallBounceEmitter.setAlpha(1, 0, 1000)
+    layerGameObjects.add(wallBounceEmitter)
     
     player1BounceEmitter = game.add.emitter(-1000, -1000, 500)
     player1BounceEmitter.makeParticles(['particle1_blue','particle2_blue','particle3_blue'])
@@ -187,6 +224,7 @@ function create() {
     player1BounceEmitter.setAlpha(1, 0, 2000)
     player1BounceEmitter.setXSpeed(0,200)
     player1BounceEmitter.setYSpeed(-500,500)
+    layerGameObjects.add(player1BounceEmitter)
 
     player2BounceEmitter = game.add.emitter(-1000, -1000, 500)
     player2BounceEmitter.makeParticles(['particle1_pink','particle2_pink','particle3_pink'])
@@ -194,6 +232,7 @@ function create() {
     player2BounceEmitter.setAlpha(1, 0, 2000)
     player2BounceEmitter.setXSpeed(-200,0)
     player2BounceEmitter.setYSpeed(-500,500)
+    layerGameObjects.add(player2BounceEmitter)
 
     traceEmitter = game.add.emitter(-1000,-1000,500)
     traceEmitter.makeParticles('trace')
@@ -202,13 +241,23 @@ function create() {
     traceEmitter.setXSpeed(0,0)
     traceEmitter.setYSpeed(0,0)
     traceEmitter.gravity = 0
+    layerGameObjects.add(traceEmitter)
 
     // pre-emit all emitters off screen to prevent initial fps drop on first emit
     emitBounceParticles(wallBounceEmitter)
     emitBounceParticles(player1BounceEmitter)
     emitBounceParticles(player2BounceEmitter)
 
+    // set up goal texts
+    goalTextPlayer1 = game.add.bitmapText(game.world.width / 2, game.world.height / 2, 'mecha_blue', 'GOAL!', 135)
+    goalTextPlayer1.anchor.set(0.5,0.5)
+    goalTextPlayer1.alpha = 0
+    layerText.add(goalTextPlayer1)
 
+    goalTextPlayer2 = game.add.bitmapText(game.world.width / 2, game.world.height / 2, 'mecha_pink', 'GOAL!', 135)
+    goalTextPlayer2.anchor.set(0.5,0.5)
+    goalTextPlayer2.alpha = 0
+    layerText.add(goalTextPlayer2)
 
     // add left-mouseklick event handler for input handling
     game.input.onDown.add(launchBall, this)
@@ -241,16 +290,33 @@ function update() {
     // collision with goals
     game.physics.arcade.collide(ball, sideWalls, function(ball, sideWalls){
         launchBall()
+        
         game.camera.shake(0.03, 1000)
+        
         if(sideWalls.name === "left"){
             scorePlayer2++
-            updateText(bmpTextScorePlayer2,scorePlayer2)
+            updateText(scoreTextPlayer2,scorePlayer2)
             emitWinParticles(player2WinEmitter)
+
+            // configure tween
+            const easing = Phaser.Easing.Exponential.In
+            const autoStart = false
+            const delay = 0
+            const repeat = 0
+            const yoyo = false
+
+            game.add.tween(goalTextPlayer2)
+            .to( { alpha: 1 }, 1, easing, autoStart, delay, repeat, yoyo)
+            .to( { alpha: 0 }, 1500, easing, autoStart, delay, repeat, yoyo)
+            .start()
+
         } else if (sideWalls.name === "right") {
             scorePlayer1++
-            updateText(bmpTextScorePlayer1,scorePlayer1)
+            updateText(scoreTextPlayer1,scorePlayer1)
             emitWinParticles(player1WinEmitter)
+            goalTextPlayer1.visible = true
         }
+
         checkScore()
     })
 
@@ -283,8 +349,8 @@ function checkScore() {
     if(scorePlayer1 === 10 || scorePlayer2 === 10) {
         scorePlayer1 = 0
         scorePlayer2 = 0
-        updateText(bmpTextScorePlayer1,scorePlayer1)
-        updateText(bmpTextScorePlayer2,scorePlayer2)
+        updateText(scoreTextPlayer1,scorePlayer1)
+        updateText(scoreTextPlayer2,scorePlayer2)
     }
 }
 
@@ -405,7 +471,8 @@ function launchBall() {
         ball.kill()
 
         ball = createBall(game.world.width / 2, game.world.height / 2)
-        
+        layerGameObjects.add(ball)
+
         ballLaunched = false
 
         paddle2.y = game.world.height / 2
